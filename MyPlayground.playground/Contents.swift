286eb1102a8006080bb3376727facb772b0b2aba
兖志飞 <yanzf@sunline.cn>
2020-01-06 14:46:27 +0800

    xxx

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -10,7 +10,7 @@ class MyViewController : UIViewController {
 
         let label = UILabel()
         label.frame = CGRect(x: 150, y: 200, width: 200, height: 20)
-        label.text = "Hello World!"
+        label.text = "Hello World!!!1111"
         label.textColor = .systemGroupedBackground
         view.addSubview(label)
         self.view = view
